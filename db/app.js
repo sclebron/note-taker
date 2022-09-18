765d32099e90c73384dc79b0850dcf091d0e09b8
SLeBron <sophieclebron@gmail.com>
2022-09-17 20:44:12 -0600

    changed port

--- db/app.js
+++ db/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 const path = require('path');
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'))
