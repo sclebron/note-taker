c57a189a64e50759df9c9cbb808a90c895892207
SLeBron <sophieclebron@gmail.com>
2022-09-21 14:41:47 -0600

    changed PORT

--- db/app.js
+++ db/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 const path = require('path');
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'))
@@ -31,6 +31,6 @@ app.post('/api/notes', (req, res) => {
   res.sendStatus(201);
 })
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`)
 })
